4ee361f5ebedce869f97890973254e200f93763f
Raj-Ailani <ailaniraj1@gmail.com>
2020-10-05 19:09:57 +0530

    ProjectSc

--- Page/js/script .js
+++ Page/js/script .js
@@ -12,6 +12,57 @@ $(document).ready(function(){
 
 
 
+    var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+var modal1 = document.getElementById("myModal1");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg1");
+var modal1Img = document.getElementById("img02");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal1.style.display = "block";
+  modal1Img.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close1")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal1.style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
     $('.carousel').carousel({
