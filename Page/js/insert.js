8b80bce28ced1b5853724882c4f1076f20ec9256
Raj-Ailani <ailaniraj1@gmail.com>
2020-10-07 13:51:48 +0530

    insert complete

--- Page/js/insert.js
+++ Page/js/insert.js
@@ -2,11 +2,24 @@ $(document).ready(function(){
       
   $("#insertbutton").click(function(){
     var email = $("#insertemail").val();
-      
+    var name = $("#insertname").val();
+    var number = $("#insertnumber").val();
+    var gender = $("#insertgender").val();
+    var age = $("#insertage").val();
+    var dl = $("#insertdl").val();
+    var aadhar = $("#insertaadhar").val();
       $.post("../Page/php/insertname.php", {
-            user : email
+
+          useremail : email,
+            username: name,
+            usernumber: number,
+            usergender : gender,
+            userage : age,
+            userdl : dl,
+            useraadhar : aadhar 
+
       } , function(data,status){
-        $("#demo").html(data);
+        $("#first").html(data);
    
       });
     });
--- Page/php/insertname.php
+++ Page/php/insertname.php
@@ -4,16 +4,47 @@
 
 </head>
 <body>
-    <h1>Done</h1>
+   
 
 
 <?php
+$host = "localhost";
+$user = "root";
+$pass = '';
+$db='cab_management';
+$con=mysqli_connect($host,$user,$pass,$db);
 
 
 
-echo "<p>" .$_POST['user'] . "</p>"
+$name =$_POST['username'];
+$email =$_POST['useremail'];
+$number =$_POST['usernumber'];
+$gender =$_POST['usergender'];
+$age =$_POST['userage'];
+$dl =$_POST['userdl'];
+$aadhar =$_POST['useraadhar'];
 
 
+$sql="INSERT INTO `driver_information` (`DriverID`, `DriverName`, `Email` ,`DriverNumber` ,`DriverGender` ,`DriverAge` ,`Driver_DL` ,`AadharCard` ) VALUES (NULL, '$name', '$email' , '$number' , '$gender','$age' , '$dl' ,'$aadhar' )";
+if ($con->query($sql) === TRUE) {
+    echo "<h3> Data Inserted successfully</h3>"
+}
+else 
+{
+    echo "failed";
+}
+
+
+
+// echo "<p>" .$name. "</p>";
+
+// echo "<p>" .$email . "</p>";
+
+// echo "<p>" .$number. "</p>";
+// echo "<p>" .$gender . "</p>";
+// echo "<p>" .$age . "</p>";
+// echo "<p>" .$dl . "</p>";
+// echo "<p>" .$aadhar . "</p>";
 
 ?>
 </body>
